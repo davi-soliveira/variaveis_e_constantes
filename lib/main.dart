a547b252094fe183d6dee23b34b0fdd6e7ecfba0
davixexeno <davi.oliveira@live.com>
2023-05-22 14:47:59 -0300

    estrutura de decisão

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:variaveis_e_constantes/caculos.dart';
 import 'package:variaveis_e_constantes/op_relacionais.dart';
 import 'media.dart';
+import 'package:variaveis_e_constantes/aluno_aprovado.dart';
 
 void main() {
-  runApp(const MaterialApp(home: OpRel()));
+  runApp(const MaterialApp(home: AlunoAprovados()));
   
 }
