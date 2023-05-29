95f09b3d59afa75527889577a940c6b9666fb7a6
davixexeno <davi.oliveira@live.com>
2023-05-28 22:30:54 -0300

    loop

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:variaveis_e_constantes/caculos.dart';
+import 'package:variaveis_e_constantes/loop.dart';
 import 'package:variaveis_e_constantes/op_logica.dart';
 import 'package:variaveis_e_constantes/op_relacionais.dart';
 import 'media.dart';
 import 'package:variaveis_e_constantes/aluno_aprovado.dart';
 
 void main() {
-  runApp(const MaterialApp(home: OpLog()));
+  runApp(const MaterialApp(home: Loop()));
   
 }
